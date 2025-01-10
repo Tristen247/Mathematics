39977bd88bda2b99076094bc3483049322a3019c
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2025-01-09 19:28:10 -0500

    Add project files.

---
+++ Mathematics.Test/BasicMathTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics.Test
+{
+    public class BasicMathTest
+    {
+        [Fact]
+        public void TestAddNumbers()
+        {
+            var math = new BasicMath();
+            var result = math.AddNumbers(5, 5);
+            Assert.True(result == 10);
+        }
+
+        [Fact]
+        public void TestSubtractNumbers()
+        {
+            var math = new BasicMath();
+            var
+
+        }
+    }
+}
